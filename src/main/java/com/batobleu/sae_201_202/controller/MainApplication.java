c990a42e4ab48bcba302c30d966ee00dcdf63844
matthis lustremant <Matthis.Lustremant@uphf.Fr>
2025-05-05 10:36:57 +0200

    Add showMap to main

--- src/main/java/com/batobleu/sae_201_202/controller/MainApplication.java
+++ src/main/java/com/batobleu/sae_201_202/controller/MainApplication.java
@@ -4,7 +4,10 @@ import com.batobleu.sae_201_202.model.Case;
 import com.batobleu.sae_201_202.model.Simulation;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,16 +15,15 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Group g = new Group();
+        Scene scene = new Scene(g, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        Simulation s = new Simulation(6,6);
-        afficherMap(s);
+        launch();
     }
 
     public static void afficherMap(Simulation s){
