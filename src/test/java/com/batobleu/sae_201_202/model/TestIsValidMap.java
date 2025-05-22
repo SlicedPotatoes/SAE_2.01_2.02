22765ac32c087deabc64af00d785ad20a815c962
Kevin M <53843746+SlicedPotatoes@users.noreply.github.com>
2025-05-22 04:05:33 +0200

    Test isValidMap

---
+++ src/test/java/com/batobleu/sae_201_202/model/TestIsValidMap.java
@@ -0,0 +1,71 @@
+package com.batobleu.sae_201_202.model;
+
+import com.batobleu.sae_201_202.exception.invalidMap.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.batobleu.sae_201_202.controller.MainController.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestIsValidMap {
+    private Simulation s;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.s = new Simulation(5, 5);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        this.s = null;
+    }
+
+    @Test
+    void test01() {
+        assertThrows(NoExitException.class, () -> {
+           this.s.isValidMap();
+        });
+    }
+
+    @Test
+    void test02() {
+        this.s.getMap()[1][0] = EXIT;
+
+        assertThrows(NoWolfException.class, () -> {
+            this.s.isValidMap();
+        });
+    }
+
+    @Test
+    void test03() {
+        this.s.getMap()[1][0] = EXIT;
+        this.s.setEntity(WOLF, 1, 1);
+
+        assertThrows(NoSheepException.class, () -> {
+            this.s.isValidMap();
+        });
+    }
+
+    @Test
+    void test04() throws InvalidMapException {
+        this.s.getMap()[1][0] = EXIT;
+        this.s.setEntity(WOLF, 1, 1);
+        this.s.setEntity(SHEEP, 1, 2);
+
+        this.s.isValidMap();
+    }
+
+    @Test
+    void test05() {
+        this.s.getMap()[1][0] = EXIT;
+        this.s.setEntity(WOLF, 1, 1);
+        this.s.setEntity(SHEEP, 1, 2);
+        this.s.getMap()[2][1] = ROCK;
+        this.s.getMap()[3][2] = ROCK;
+
+        assertThrows(UnconnectedGraphException.class, () -> {
+           this.s.isValidMap();
+        });
+    }
+}
