ece0d8b02b9255a2c348c53af9f3aa9168a45867
Matthis <matthis.lustremant@uphf.fr>
2025-06-05 12:47:10 +0200

    BFS Finish

--- src/main/java/com/batobleu/sae_201_202/model/algo/BfsMatthis.java
+++ src/main/java/com/batobleu/sae_201_202/model/algo/BfsMatthis.java
@@ -5,9 +5,7 @@ import com.batobleu.sae_201_202.model.entity.Entity;
 import com.batobleu.sae_201_202.model.tile.*;
 import javafx.util.Pair;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import static com.batobleu.sae_201_202.controller.MainController.*;
 
@@ -19,7 +17,7 @@ public class BfsMatthis extends PathFinding{
     public BfsMatthis() {
     }
 
-    public ArrayList<Pair<Integer, Integer>> voisins(int x, int y) {
+    public ArrayList<Pair<Integer, Integer>> voisins(int y, int x) {
         ArrayList<Pair<Integer, Integer>> v = new ArrayList<>();
         if (y > 0 && map[y - 1][x] != ROCK) {
             v.add(new Pair<>(y - 1, x));
@@ -37,22 +35,24 @@ public class BfsMatthis extends PathFinding{
     }
 
     public ArrayList<Pair<Integer, Integer>> getChemin() {
+        //initialisation
         Pair<Integer, Integer> start = new Pair<>(this.sim.getSheep().getY(), this.sim.getSheep().getX());
         ArrayList<Pair<Integer, Integer>> visited = new ArrayList<>();
         ArrayList<Pair<Integer, Integer>> queue = new ArrayList<>();
         HashMap<Pair<Integer, Integer>,Pair<Integer, Integer>> precedent = new HashMap<>();
         precedent.put(start, null);
-
         queue.add(start);
         visited.add(start);
         List<Integer> temp = this.sim.findExitMapTile();
         Pair<Integer, Integer> exit = new Pair<>(temp.get(1), temp.get(0));
-        while (!queue.isEmpty() || !visited.contains(start)) {
+
+        //Parcours
+        while (!queue.isEmpty()) {
             Pair<Integer, Integer> curr = queue.getFirst();
             if (curr == exit) {
                 break;
             }
-            ArrayList<Pair<Integer, Integer>> v = voisins(curr.getValue(), curr.getKey());
+            ArrayList<Pair<Integer, Integer>> v = voisins(curr.getKey(), curr.getValue());
             for (Pair<Integer, Integer> p1 : v) {
                 if (!visited.contains(p1)) {
                     visited.add(p1);
@@ -63,16 +63,20 @@ public class BfsMatthis extends PathFinding{
             queue.remove(curr);
 
         }
+
+        //Recherche du chemin
         ArrayList<Pair<Integer, Integer>> chemin = new ArrayList<>();
         Pair<Integer, Integer> courant = exit;
         while (courant != null) {
-            Pair<Integer, Integer> a = courant;
+            Pair<Integer, Integer> ancien = courant;
             courant = precedent.get(courant);
             if (courant != null) {
-                chemin.add(new Pair<>(a.getValue() - courant.getValue(), a.getKey() - courant.getKey()));
+                chemin.add(new Pair<>(ancien.getValue() - courant.getValue(), ancien.getKey() - courant.getKey()));
             }
         }
 
+        //Inversion du chemin
+        Collections.reverse(chemin);
         return chemin;
     }
 
@@ -80,7 +84,11 @@ public class BfsMatthis extends PathFinding{
     public Pair<Integer, Integer> nextMove(Simulation sim) {
         this.sim = sim;
         this.map = sim.getMap();
-        this.chemin = getChemin();
+
+        if (this.chemin == null || this.chemin.isEmpty()) {
+            this.chemin = getChemin();
+        }
+
         Pair<Integer, Integer> a = this.chemin.getFirst();
         this.chemin.remove(a);
         return a;
